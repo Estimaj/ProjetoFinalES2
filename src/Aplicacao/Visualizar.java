fdde48e346f130daf111a5f5e50c7df5da9d9e30
ferreira011 <estgv16294@alunos.estgv.ipv.pt>
2021-06-11 15:44:22 +0100

    testes para API e Replicas

--- src/Aplicacao/Visualizar.java
+++ src/Aplicacao/Visualizar.java
@@ -9,7 +9,7 @@ public class Visualizar {
 
     public boolean verificarPodeLer(Emprestimo emprestimo, LocalDate dataLeitura) throws NotAllowedToReadException {
 
-        // blackbox particionamento + VF
+        // blackbox particionamento
         if(!emprestimo.getUtilizador().getEstado_utilizador().equals("ativo") || dataLeitura.isBefore(emprestimo.getDataHoraEmp()) ||dataLeitura.isAfter(emprestimo.getFimdataHoraEmp())) {
             System.out.println("O utilizador não tem permissão para ler este livro!");
             throw new NotAllowedToReadException("O utilizador não tem permissão para ler este livro!");
